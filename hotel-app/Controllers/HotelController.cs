43347e5bc2d15602e19bf1cf79a15c5ff62af147
mo3liii <mustafasoliman19@gmail.com>
2024-03-21 19:54:52 +0200

    start register

---
+++ hotel-app/Controllers/HotelController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace hotel_app.Controllers
+{
+	public class HotelController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult Register()
+		{
+			return View("Register");
+		}
+
+	}
+}
---
+++ hotel-app/ViewModels/HotelUserRegiterVM.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace hotel_app.ViewModels
+{
+	public class HotelUserRegiterVM
+	{
+		public string Name { get; set; }
+		public string? Description { get; set; }
+		public string Country { get; set; } //from combobox in UI
+		public string City { get; set; } //from combobox in UI based on country
+		public string? Address { get; set; } //street,area...
+		public int StarRating { get; set; } // 4,5,.. starts
+
+		[ForeignKey("HotelCategory")]
+		public int Category { get; set; } //luxery , resort , standard.. 
+		public bool? IsAccessible { get; set; }
+		public string UserName { get; set; }
+
+		[DataType(DataType.Password)]
+		public string Password { get; set; }
+
+		[DataType(DataType.Password)]
+		[Compare("Password")]
+		public string ConfirmPassword { get; set; }
+
+	}
+}
