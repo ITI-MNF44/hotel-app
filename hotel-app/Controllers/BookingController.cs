1a8b9cd94b08526a1b5f95d28a95a1cef6fa9889
FatmaSorour8 <fatmamohamed862s@outlook.com>
2024-03-21 22:14:28 +0200

    Stablish main layout...

---
+++ hotel-app/Controllers/BookingController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace hotel_app.Controllers
+{
+    public class BookingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
