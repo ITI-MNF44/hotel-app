119326c1a3de3b55fb2ff15d3c754ca04debc741
ola <olanazmy@gmail.com>
2024-03-23 03:11:12 +0200

    hotel registeration done

--- hotel-app/ViewModels/HotelViewModel.cs
+++ hotel-app/ViewModels/HotelViewModel.cs
@@ -1,21 +1,23 @@
-﻿namespace hotel_app.ViewModels
+﻿using hotel_app.Models;
+
+namespace hotel_app.ViewModels
 {
     public class HotelViewModel
     {
-        public int Id { get; set; }
+        //public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public string Country { get; set; }
         public string City { get; set; }
         public string Address { get; set; }
-        public IFormFile Image { get; set; }
-        public int StarRating { get; set; }
-        public string Category { get; set; }
-        public DateTime? CreatedDate { get; set; }
-        public bool? IsAccessible { get; set; }
-        public bool? IsWorking { get; set; }
+        public string Image { get; set; }
         public double? Latitude { get; set; }
         public double? Longitude { get; set; }
-        public bool IsDeleted { get; set; }
+        public int StarRating { get; set; }
+        public int Category { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public List<Room>? Rooms { get; set; }
+        public List<Food>? Foods { get; set; }
+     
     }
 }
